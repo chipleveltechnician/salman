4cff77b099065f93aeaf7e2d6b1f3436c93fabbe
Salmanul Faris MS <fazzfaisu@gmail.com>
2026-09-21 19:58:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/', // This forces Vercel to look in the right place
 })
